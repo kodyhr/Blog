a2bf9e3dca3b3725a732e19e0254180e8aa475a7
kodyhr <79831696+kodyhr@users.noreply.github.com>
2021-04-05 00:39:23 -1000

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const _ = require("lodash");
 // required to access the Weather API
 const https = require("https");
 
+
 const homeStartingContent = "Introductory content for my blog. This is the starting page for my daily journal.";
+const weatherContent = "Please enter a city name to view results of weather.";
 const aboutContent = "This page describes what the blog is all about and some information about myself.";
 const contactContent = "Brief information about how to contact me via social media.";
 
@@ -27,6 +29,7 @@ app.get("/", function(req, res){
     posts: posts
     });
 });
+
 
 app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent});
@@ -67,6 +70,54 @@ app.get("/posts/:postName", function(req, res){
   });
 
 });
+app.get("/weather", function(req, res){
+  res.render("weather", {weatherContent: weatherContent});
+});
+
+
+//displays index.html of root path
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/weather.ejs")
+});
+
+//invoked after hitting go in the html form
+app.post("/", function(req, res) {
+    
+    // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
+        var city = String(req.body.cityInput);
+        console.log(req.body.cityInput);
+    
+    //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
+        const units = "imperial";
+        const apiKey = "509edaf8edd69f1612201dbd2775f72c";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&APPID=" + apiKey;
+    
+    // this gets the data from Open WeatherPI
+    https.get(url, function(response){
+        console.log(response.statusCode);
+        
+        // gets individual items from Open Weather API
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const city = weatherData.name;
+            const humidity = weatherData.main.humidity;
+            const windD = weatherData.wind.deg;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            
+            // displays the output of the results
+            res.write("<h1> The weather is " + weatherDescription + "<h1>");
+            res.write("<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write("<h2>The Humidity is: "+ humidity + "%<h2> ");
+            res.write("<h2>Wind Direction is: " + windD +" degrees<h2>");
+            res.write("<img src=" + imageURL +">");
+            res.send();
+        });
+    });
+})
+ 
 
 // need to write out an app.get function that open a route /weather Need an EJS view called weather.ejs that displays one text field to input city name
 //This EJS view will input a city name from user
